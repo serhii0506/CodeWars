52810e4f810fdd9f73a8e03052ec118978ff2a0d
Serhii Yakovenko <serhii_yakovenko@epam.com>
2022-07-17 13:00:22 +0300

    tast complete

--- src/main/java/kyu4/DoubleLinear.java
+++ src/main/java/kyu4/DoubleLinear.java
@@ -1,9 +1,27 @@
 package kyu4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DoubleLinear {
-    
-    public static int dblLinear (int n) {
-        
-    	return 0;
-    }
+
+	public static int dblLinear(int n) {
+		if (n == 0) return 1;
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		recursion(list, list, n);
+		return list.stream().distinct().toList().get(n);
+	}
+
+	private static void recursion( List<Integer> before, List<Integer> list, int n) {
+		List<Integer> next = new ArrayList<>();
+		for(int i : before) {
+			next.add(i * 2 + 1);
+			next.add(i * 3 + 1);
+		}
+		list.addAll(next);
+		Collections.sort(list);
+		if (list.size() < n*6) recursion(next, list, n);
+	}
 }
