44469a857b4bea8838008e0b8ce7f73b4d1baab9
Serhii Yakovenko <serhii_yakovenko@epam.com>
2022-07-18 18:15:53 +0300

    task Pyramid Slide Down complete

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CodeWars
 Repository for solved tasks from www.codewars.com
 
-* 18      :  kuy4 - Pyramid Slide Down (LongestSlideDown)
+* 18.07.22:  kuy4 - Pyramid Slide Down (LongestSlideDown)
 * 17.07.22:  kyu4 - Twice linear
 * 16.07.22:  kyu4 - Mixing
 * 16.07.22:  kyu4 - ObserverPin
--- src/main/java/kyu4/LongestSlideDown.java
+++ src/main/java/kyu4/LongestSlideDown.java
@@ -1,9 +1,15 @@
 package kyu4;
 
 public class LongestSlideDown {
+	    public static int longestSlideDown(int[][] pyramid) {
+	        int[][] max = pyramid;
 
-    public static int longestSlideDown(int[][] pyramid) {
-        // Code Goes Here..
-    	return 0;
-    }
+	        for (int y = pyramid.length-2; y >= 0; y--) {
+	            for (int x = 0; x < pyramid[y].length; x++) {
+	                max[y][x] = pyramid[y][x] + Math.max(pyramid[y + 1][x], pyramid[y + 1][x + 1]);
+	            }
+	        }
+
+	        return max[0][0];
+	    }
 }
