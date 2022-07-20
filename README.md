ca2546ab0734bb06e52cbc3310bc6f7d840bd319
Serhii Yakovenko <109414649+serhii0506@users.noreply.github.com>
2022-07-20 17:51:07 +0300

    add Most frequently used words in a text complete

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CodeWars
 Repository for solved tasks from www.codewars.com
 
+* 20.07.22:  kyu4 - Most frequently used words in a text(Serhii Yakovenko)
 * 19.07.22:  kuy4 - Roman Numerals Helper(Serhii Yakovenko)
 * 19.07.22:  kuy4 - MatrixDeterminant(Serhii Yakovenko)
 * 18.07.22:  kuy4 - Pyramid Slide Down(Serhii Yakovenko)
--- src/main/java/allCode/OurGitConflicts.java
+++ src/main/java/allCode/OurGitConflicts.java
@@ -3,12 +3,36 @@ package allCode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class OurGitConflicts {
-	
-	
+
+	/**
+	 * kyu4 - Most frequently used words in a text
+	 * 
+	 * @author Serhii Yakovenko.
+	 * 
+	 */
+	public static List<String> top3(String s) {
+		String[] array = s.toLowerCase().replaceAll("(?i)[^А-ЯЁA-Z']+", " ").split(" ");
+		Map<String, Integer> words = new TreeMap<>();
+		for (String str : array) {
+			if (str.matches("\\w+('\\w*)*")) {
+				if (words.containsKey(str)) {
+					words.put(str, words.get(str) + 1);
+				} else {
+					words.put(str, 1);
+				}
+			}
+		}
+		return words.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(3)
+				.map(e -> e.getKey()).collect(Collectors.toList());
+	}
+
 	/**
 	 * kyu4 - Roman Numerals Helper
 	 * 
@@ -93,7 +117,7 @@ public class OurGitConflicts {
 		}
 		return count;
 	}
-	
+
 	/**
 	 * kyu4 - MatrixDeterminant
 	 * 
@@ -101,46 +125,48 @@ public class OurGitConflicts {
 	 * 
 	 */
 	public static int determinant(int[][] matrix) {
-    	int det = 0;
-    	if(matrix.length == 1) return matrix[0][0];
-    	if(matrix.length == 2) return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
-    	else {
-    		for(int j = 0; j < matrix.length; j++) {
-    			int[][] newMatrix = new int[matrix.length-1][matrix.length-1];
-    			for(int y = 0; y < newMatrix.length; y++) {
-    				int plus = 0;
-    				for(int x = 0; x < newMatrix.length; x++) {
-    					if(x == j) plus++;
-        				newMatrix[y][x] = matrix[y+1][x+plus];
-        			}
-    			}
-    			det = det + (int) (Math.pow(-1, j+2)) * matrix[0][j] * determinant(newMatrix);
-    		}
-    		
-    	}
-        
-        return det;
-    }
-	
+		int det = 0;
+		if (matrix.length == 1)
+			return matrix[0][0];
+		if (matrix.length == 2)
+			return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+		else {
+			for (int j = 0; j < matrix.length; j++) {
+				int[][] newMatrix = new int[matrix.length - 1][matrix.length - 1];
+				for (int y = 0; y < newMatrix.length; y++) {
+					int plus = 0;
+					for (int x = 0; x < newMatrix.length; x++) {
+						if (x == j)
+							plus++;
+						newMatrix[y][x] = matrix[y + 1][x + plus];
+					}
+				}
+				det = det + (int) (Math.pow(-1, j + 2)) * matrix[0][j] * determinant(newMatrix);
+			}
+
+		}
+
+		return det;
+	}
+
 	/**
 	 * kyu4 - Pyramid Slide Down
 	 * 
 	 * @author Serhii Yakovenko.
 	 * 
 	 */
-	 public static int longestSlideDown(int[][] pyramid) {
-	        int[][] max = pyramid;
+	public static int longestSlideDown(int[][] pyramid) {
+		int[][] max = pyramid;
 
-	        for (int y = pyramid.length-2; y >= 0; y--) {
-	            for (int x = 0; x < pyramid[y].length; x++) {
-	                max[y][x] = pyramid[y][x] + Math.max(pyramid[y + 1][x], pyramid[y + 1][x + 1]);
-	            }
-	        }
+		for (int y = pyramid.length - 2; y >= 0; y--) {
+			for (int x = 0; x < pyramid[y].length; x++) {
+				max[y][x] = pyramid[y][x] + Math.max(pyramid[y + 1][x], pyramid[y + 1][x + 1]);
+			}
+		}
 
-	        return max[0][0];
-	    }
-	
-	
+		return max[0][0];
+	}
+
 	/**
 	 * kyu4 - SumOfDivided
 	 * 
@@ -151,22 +177,27 @@ public class OurGitConflicts {
 		int max = Arrays.stream(l).map(e -> Math.abs(e)).max().orElse(-1);
 		List<Integer> simpleDeviders = new ArrayList<>();
 		for (int i = 2; i <= max; i++) {
-			if (checkSimple(i)) simpleDeviders.add(i);
+			if (checkSimple(i))
+				simpleDeviders.add(i);
 		}
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < simpleDeviders.size(); i++) {
 			int devider = simpleDeviders.get(i);
 			int[] elements = Arrays.stream(l).filter(e -> e % devider == 0).toArray();
 			int sum = Arrays.stream(elements).sum();
-			if (sum != 0 || elements.length > 0) sb.append("(" + devider + " " + sum + ")");
+			if (sum != 0 || elements.length > 0)
+				sb.append("(" + devider + " " + sum + ")");
 		}
 		return sb.toString();
 	}
 
 	public static boolean checkSimple(int i) {
-		if (i <= 1)	return false;
-		else if (i <= 3) return true;
-		else if (i % 2 == 0 || i % 3 == 0) return false;
+		if (i <= 1)
+			return false;
+		else if (i <= 3)
+			return true;
+		else if (i % 2 == 0 || i % 3 == 0)
+			return false;
 		int n = 5;
 		while (n * n <= i) {
 			if (i % n == 0 || i % (n + 2) == 0)
@@ -175,7 +206,7 @@ public class OurGitConflicts {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * kyu4 - Twice Linear
 	 * 
@@ -183,25 +214,26 @@ public class OurGitConflicts {
 	 * 
 	 */
 	public static int dblLinear(int n) {
-		if (n == 0) return 1;
+		if (n == 0)
+			return 1;
 		List<Integer> list = new ArrayList<>();
 		list.add(1);
 		recursion(list, list, n);
 		return list.stream().distinct().toList().get(n);
 	}
 
-	private static void recursion( List<Integer> before, List<Integer> list, int n) {
+	private static void recursion(List<Integer> before, List<Integer> list, int n) {
 		List<Integer> next = new ArrayList<>();
-		for(int i : before) {
+		for (int i : before) {
 			next.add(i * 2 + 1);
 			next.add(i * 3 + 1);
 		}
 		list.addAll(next);
 		Collections.sort(list);
-		if (list.size() < n*6) recursion(next, list, n);
+		if (list.size() < n * 6)
+			recursion(next, list, n);
 	}
-	
-	
+
 	/**
 	 * kyu4 - Mixing
 	 * 
@@ -227,58 +259,59 @@ public class OurGitConflicts {
 						list.add("2:" + String.valueOf(letter).repeat(arr2[j]));
 					} else if (arr2[j] == arr1[j]) {
 						list.add("=:" + String.valueOf(letter).repeat(i));
-						
+
 					}
-					arr1[j]=arr2[j]=0;
+					arr1[j] = arr2[j] = 0;
 				} else if (arr2[j] == i) {
 					char letter = (char) (j + 97);
 					list.add("2:" + String.valueOf(letter).repeat(arr2[j]));
-					arr1[j]=arr2[j]=0;
+					arr1[j] = arr2[j] = 0;
 				}
 			}
 		}
 		List<String> list2 = new ArrayList<>();
-		for(int i = max+2; i>3; i--) {
+		for (int i = max + 2; i > 3; i--) {
 			int length = i;
 			List<String> before = list.stream().filter(e -> e.length() == length).collect(Collectors.toList());
 			Collections.sort(before);
-			before.forEach(e->list2.add(e));
+			before.forEach(e -> list2.add(e));
 		}
 		return String.join("/", list2);
 	}
-	
-	
+
 	/**
 	 * kyu4 - ObserverPin
 	 * 
 	 * @author Serhii Yakovenko.
 	 * 
 	 */
-		public static List<String> getPINs(String observed) {
-			int[][] arr = new int[][] { { 0, 8 }, { 1, 2, 4 }, { 1, 2, 3, 5 }, { 2, 3, 6 },
-				{ 1, 4, 5, 7 }, { 2, 4, 5, 6, 8 }, { 3, 5, 6, 9 }, { 4, 7, 8 }, { 5, 7, 8, 9, 0 }, { 6, 8, 9 } };
-			int el = Integer.parseInt(String.valueOf(observed.charAt(0)));
-			if(observed.length() == 1) return Arrays.stream(arr[el]).boxed().map(e->String.valueOf(e)).collect(Collectors.toList());
-			List<String> result = new ArrayList<String>();
-				for (int i1 : arr[el]) {
-					String combo = i1 + observed.substring(1);
-					result.add(combo);
-					rec(combo, 1, result, arr);
-				}		
-			return result.stream().distinct().collect(Collectors.toList());
+	public static List<String> getPINs(String observed) {
+		int[][] arr = new int[][] { { 0, 8 }, { 1, 2, 4 }, { 1, 2, 3, 5 }, { 2, 3, 6 }, { 1, 4, 5, 7 },
+				{ 2, 4, 5, 6, 8 }, { 3, 5, 6, 9 }, { 4, 7, 8 }, { 5, 7, 8, 9, 0 }, { 6, 8, 9 } };
+		int el = Integer.parseInt(String.valueOf(observed.charAt(0)));
+		if (observed.length() == 1)
+			return Arrays.stream(arr[el]).boxed().map(e -> String.valueOf(e)).collect(Collectors.toList());
+		List<String> result = new ArrayList<String>();
+		for (int i1 : arr[el]) {
+			String combo = i1 + observed.substring(1);
+			result.add(combo);
+			rec(combo, 1, result, arr);
 		}
+		return result.stream().distinct().collect(Collectors.toList());
+	}
 
-		private static void rec(String newString, int index, List<String> list, int[][] arr) {
-				int el = Integer.parseInt(String.valueOf(newString.charAt(index)));
-				for (int i1 : arr[el]) {
-					String before = newString.substring(0, index);
-					String after = "";
-					if (newString.length() - index >= 2)
-						after = newString.substring(index+1);
-					String combo = before + i1 + after;
-					list.add(combo);
-					if(index < newString.length()-1) rec(combo, index+1, list, arr);
-				}		
+	private static void rec(String newString, int index, List<String> list, int[][] arr) {
+		int el = Integer.parseInt(String.valueOf(newString.charAt(index)));
+		for (int i1 : arr[el]) {
+			String before = newString.substring(0, index);
+			String after = "";
+			if (newString.length() - index >= 2)
+				after = newString.substring(index + 1);
+			String combo = before + i1 + after;
+			list.add(combo);
+			if (index < newString.length() - 1)
+				rec(combo, index + 1, list, arr);
 		}
+	}
 
 }
