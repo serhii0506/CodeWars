63d5f0e7d184619ffe5e003834f432e15a699fa7
Serhii Yakovenko <serhii_yakovenko@epam.com>
2022-07-20 17:49:17 +0300

    task Most frequently used words in a text complete

--- src/test/java/allCode/OurGitConflictsTest.java
+++ src/test/java/allCode/OurGitConflictsTest.java
@@ -8,12 +8,39 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class OurGitConflictsTest {
 
+	/**
+	 * kyu4 - Most frequently used words in a text
+	 * 
+	 * @author Serhii Yakovenko.
+	 * 
+	 */
+	 @Test
+	  public void top3Test() {
+	    assertEquals(Arrays.asList("e", "d", "a"),    OurGitConflicts.top3("a a a  b  c c  d d d d  e e e e e"));
+	    assertEquals(Arrays.asList("e", "ddd", "aa"), OurGitConflicts.top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
+	    assertEquals(Arrays.asList("won't", "wont"),  OurGitConflicts.top3("  //wont won't won't "));
+	    assertEquals(Arrays.asList("e"),              OurGitConflicts.top3("  , e   .. "));
+	    assertEquals(Arrays.asList(),                 OurGitConflicts.top3("  ...  "));
+	    assertEquals(Arrays.asList(),                 OurGitConflicts.top3("  '  "));
+	    assertEquals(Arrays.asList(),                 OurGitConflicts.top3("  '''  "));
+	    assertEquals(Arrays.asList("a", "of", "on"),  OurGitConflicts.top3(Stream
+	                    .of("In a village of La Mancha, the name of which I have no desire to call to",
+	                        "mind, there lived not long since one of those gentlemen that keep a lance",
+	                        "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+	                        "coursing. An olla of rather more beef than mutton, a salad on most",
+	                        "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+	                        "on Sundays, made away with three-quarters of his income.")
+	                     .collect(Collectors.joining("\n")) )); 
+	  }
+	
 	
 	/**
 	 * kyu4 - Roman Numerals Helper
